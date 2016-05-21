8b3bd38c362f93839ecb14616b74ed0de502c162
vitaliy-pavlenko <theatre_of_illusion@mail.ru>
2016-05-21 16:53:55 +0300

    added progress bar (task #2)

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,14 @@
 require('../styles/rating.styl');
 require('../styles/btn.styl');
+require('../styles/progress.polyfill.styl');
+require('../styles/progress-bar.styl');
 
 require('./matches.polyfill');
 require('./rating');
 require('./btn');
+require('./progress.polyfill');
 
 //инициализация рейтинг-панели
 RatingPanel.init('.rating-block');
+
+document.createElement('progress');
