e2f85f87f80fea6e6965a607faec510b573ddcd3
vitaliy-pavlenko <theatre_of_illusion@mail.ru>
2016-05-23 09:49:33 +0300

    added page layout (task #4)

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ require('../styles/rating.styl');
 require('../styles/btn.styl');
 require('../styles/progress.polyfill.styl');
 require('../styles/progress-bar.styl');
+require('../styles/page.styl');
 
 require('./matches.polyfill');
 require('./rating');
